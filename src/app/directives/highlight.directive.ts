fab993bb5d5d7832da14d556bb86721882db4b0b
Horia Radu <horia.radu23@gmail.com>
2017-03-14 12:02:11 +0200

    highlight directive - host binding

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,13 +1,27 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  private backgroundColor = 'white';
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer) {
-    // this.elementRef.nativeElement.style.backgroundColor = 'green';
-    this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');
+  @HostBinding('style.backgroundColor')
+  get color() {
+    return this.backgroundColor;
+  }
+
+  @HostListener('mouseenter')
+  mouseenter() {
+    this.backgroundColor = 'green';
+  };
+
+  @HostListener('mouseleave')
+  mouseleave() {
+    this.backgroundColor = 'white';
+  };
+
+  constructor() {
   }
 
 }
