9ea44a186c1a15285861717fa721ed00212b2396
Horia Radu <horia.radu23@gmail.com>
2017-03-16 13:48:28 +0200

    add forms
    - data driven with user defined input

--- src/app/forms/data-driven/data-driven.component.ts
+++ src/app/forms/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'data-driven',
@@ -15,23 +15,44 @@ export class DataDrivenComponent implements OnInit {
 
   genders = ['male', 'female'];
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
-    this.myForm = new FormGroup({
-      userData: new FormGroup({
-        username: new FormControl('Horia', Validators.required),
-        email: new FormControl('horia@radu.com',
+    // this.myForm = new FormGroup({
+    //   userData: new FormGroup({
+    //     username: new FormControl('Horia', Validators.required),
+    //     email: new FormControl('horia@radu.com',
+    //       [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])+")]
+    //     )
+    //   }),
+    //   password: new FormControl('', Validators.required),
+    //   gender: new FormControl('male'),
+    //   hobbies: new FormArray([
+    //     new FormControl('Cooking', Validators.required)
+    //   ])
+    // });
+
+    this.myForm = this.formBuilder.group({
+      userData: this.formBuilder.group({
+        username: ['Horia', Validators.required],
+        email: ['horia@radu.com',
           [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])+")]
-        )
+        ]
       }),
-      password: new FormControl('', Validators.required),
-      gender: new FormControl('male')
+      password: ['', Validators.required],
+      gender: ['male'],
+      hobbies: this.formBuilder.array([
+        ['Cooking', Validators.required]
+      ])
     })
   }
 
   onSubmit() {
     console.log(this.myForm);
+  }
+
+  addHobby() {
+    (<FormArray>this.myForm.controls['hobbies']).push(new FormControl('', Validators.required));
   }
 }
